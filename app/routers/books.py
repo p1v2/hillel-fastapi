27cd64fc71c015b5883a9814851592378ce9f39e
VsevolodSheyko <dyeji9iht@gmail.com>
2023-05-07 18:03:05 +0300

    Create update, get and delete endpoints

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,6 +1,6 @@
 import json
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 
 from app.dependencies.db import get_session
@@ -29,14 +29,18 @@ class BookCreateModel(BaseModel):
     author: str
 
 
+class BookUpdate(BaseModel):
+    title: str = None
+    author: str = None
+    description: str = None
+    pages_count: int = None
+
+
 @router.get("", response_model=list[BookModel])
 async def get_books(title: str = None, session=Depends(get_session)):
     books = session.query(Book).all()
 
-    books_models = []
-    for book in books:
-        books_models.append(BookModel.from_orm(book))
-    return books_models
+    return [BookModel.from_orm(book) for book in books]
 
 
 # @router.get("/{book_id}", response_model=BookModel)
@@ -57,3 +61,28 @@ async def create_book(book: BookCreateModel, session=Depends(get_session)):
 
     return BookModel.from_orm(book)
 
+
+@router.put("/books/{book_id}", response_model=BookModel, status_code=200)
+async def update_book(book_id: int, book_update: BookUpdate, session=Depends(get_session)):
+    for var, value in vars(book_update).items():
+        if value is not None:
+            setattr(book_id, var, value)
+    session.commit()
+    
+
+@router.delete("/books/{book_id}", response_model=BookModel)
+async def delete_book(book_id: int, session=Depends(get_session)):
+    book = session.query(Book).filter(Book.id == book_id).first()
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    session.delete(book)
+    session.commit()
+    return {"message": "Book deleted"}
+
+
+@router.get("/books/{book_id}", response_model=BookModel)
+async def get_book(book_id: int, session=Depends(get_session)):
+    if book := session.query(Book).filter(Book.id == book_id).first():
+        return Book(**book.__dict__)
+    else:
+        raise HTTPException(status_code=404, detail="Book not found")
